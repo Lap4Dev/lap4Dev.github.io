4e4f87decd9d05d9a116cbee3804aee9ffe999e7
Olexandr <o.lapchuk@nltu.lviv.ua>
2022-08-09 01:58:19 +0300

    2

--- src/App.js
+++ src/App.js
@@ -6,38 +6,39 @@ import '@vkontakte/vkui/dist/vkui.css';
 import Home from './panels/Home';
 import Persik from './panels/Persik';
 
-const qs = require('querystring');
-const crypto = require('crypto');
+import verifyLaunchParams from './signUser.js';
+// import bridge from '@vkontakte/vk-bridge';
+const secretKeyOfYourApp = 'W11Q4F7GS2721s7cJWEi'
+
+async function startCheck() {
+	// Таким образом мы можем получить URL, который нам нужен для верификации пользователя :)
+	const URL = globalThis.location.href;
+	console.log(URL, 'here');
+	// Имиортируем ту самую функцию 'verifyLaunchParams' из файла 'signUser.js'. 
+	// Передаем мы URL с параметрами и секретный ключ нашего приложения, который находится в настройках самого же приложения. 
+	console.log(verifyLaunchParams(URL, secretKeyOfYourApp) ? 'Успешно подтверждено :)' : 'Не подтверждено :(');
+	const user = await bridge.send('VKWebAppGetUserInfo'); // Обращаемся к bridge и отправляем событие "VKWebAppGetUserInfo".
+	console.log('Проверка связи')
+	// В ответ мы получаем объект с данными о пользователе
+	const userId = user.id; // Тут я достаю ID пользователя
+	const first_name = user?.first_name; // Тут я достаю имя пользователя
+	const last_name = user?.last_name; // Тут я достаю фамилию пользователя
+	const country = user?.country?.title; // Тут я достаю страну пользователя
+	const city = user?.city?.title; // Тут я достаю город пользователя
+	const photos = []; // Сюда буду складывать URLs фотографий
+	for (key in user) key.includes('photo') && photos.push(user[key]); // Если есть ключ, который содержит 'photo', я добавляю его в масиив 'photos'
+	// Также вам нужно учти тот факт, если каких-то данных о пользователе не будет, тогда переменная примет 'undefined'. Вы можете делать проверку
+	//если это нужно.
+	
+}
 
 const App = () => {
 	const [scheme, setScheme] = useState('bright_light')
 	const [activePanel, setActivePanel] = useState('home');
 	const [fetchedUser, setUser] = useState(null);
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-	console.log("HELLO")
-	console.log(globalThis.location.href)
-	
-
-	const urlParams = qs.parse(URL_PARAMS);
-	const signKeys = new URLSearchParams(URL_PARAMS)
-		.get('sign_keys')
-		.split(',');
-
-	const ordered = {};
-
-	signKeys.forEach(key => ordered[key] = urlParams[key]);
-
-	const stringParams = qs.stringify(ordered);
-	const paramsHash = crypto
-		.createHmac('sha256', secretKey)
-		.update(stringParams)
-		.digest()
-		.toString('base64')
-		.replace(/\+/g, '-')
-		.replace(/\//g, '_')
-		.replace(/=$/, '');
-
-	console.log(paramsHash === urlParams.sign);
+	// Запускаем проверку
+	startCheck()
 	useEffect(() => {
 		bridge.subscribe(({ detail: { type, data }}) => {
 			if (type === 'VKWebAppUpdateConfig') {
@@ -48,7 +49,7 @@ const App = () => {
 
 		async function fetchData() {
 			const user = await bridge.send('VKWebAppGetUserInfo');
-			console.log(user)
+			startCheck()
 			setUser(user);
 			setPopout(null);
 		}
---
+++ vk-bridge/index.js
@@ -0,0 +1,4 @@
+import bridge from '@vkontakte/vk-bridge';
+
+bridge.send("VKWebAppInit", {});
+bridge.subscribe((e) => console.log(e)); 
